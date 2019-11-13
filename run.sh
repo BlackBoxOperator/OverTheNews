a3a330619550b12d2987bdc359676c112894c94f
nobodyzxc <dici86.chen@gmail.com>
2019-11-14 03:08:11 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if grep Arch /etc/os-release;then
+if grep Arch /etc/os-release > /dev/null;then
     INSTALL="sudo pacman -S "
-elif grep Ubuntu /etc/os-release;then
+elif grep Ubuntu /etc/os-release > /dev/null;then
     INSTALL="sudo apt install "
 fi
 
@@ -28,39 +28,30 @@ check_cmd python3.6 \
     fi"
 check_cmd pip3.6 "curl https://bootstrap.pypa.io/get-pip.py | sudo python3.6"
 
-# download_from_gdrive <FILE_ID> <OUTPUT_FILENAME>
 download_from_gdrive() {
     file_id=$1
     file_name=$2 # first stage to get the warning html
-    curl -c /tmp/cookies \
-    "https://drive.google.com/uc?export=download&id=$file_id" > \
-    /tmp/intermezzo.html
-    # second stage to extract the download link from html above
-    download_link=$(cat /tmp/intermezzo.html | \
-    grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | \
-    sed 's/\&amp;/\&/g')
-    curl -L -b /tmp/cookies \
-    "https://drive.google.com$download_link" > $file_name
-}
-
-down(){
-    case $1 in
-        "large") download_from_gdrive $2 $3 ;;
-        "small") wget --no-check-certificate \
-            "https://drive.google.com/uc?export=download&id=$2" -O $3 ;;
-        *)  echo 'invalid option'; exit ;;
-    esac
+    curl -L -o $file_name -c /tmp/cookies \
+    "https://drive.google.com/uc?export=download&id=$file_id"
+    if grep "Virus scan warning" $file_name > /dev/null;then
+        # second stage to extract the download link from html above
+        download_link=$(cat $file_name | \
+        grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | \
+        sed 's/\&amp;/\&/g')
+        curl -L -b /tmp/cookies \
+        "https://drive.google.com$download_link" > $file_name
+    fi
 }
 
 # download corpus files
-down small 1avkR7mu2mMnIcynhqEGc0ftnMFfBwHDA corpus/60w_title.txt
-down large 1VwjLCVfByaO5ywBFg_nmjp9RlJBRevCk corpus/60w_token.txt
-down small 1Eveuc2Yd12ehBmM-pmNde4ij1IUXDRMF corpus/60w_tokey.txt
+download_from_gdrive 1avkR7mu2mMnIcynhqEGc0ftnMFfBwHDA corpus/60w_title.txt
+download_from_gdrive 1VwjLCVfByaO5ywBFg_nmjp9RlJBRevCk corpus/60w_token.txt
+download_from_gdrive 1Eveuc2Yd12ehBmM-pmNde4ij1IUXDRMF corpus/60w_tokey.txt
 
 # download word2vector models
-down small 1oJZGdpu2Mm-Ga13H_5qxJeZ-nsHjn-NM w2v/news_d200_e100.w2v
-down large 11h5ZFbVAsqd41YnmM11TSZia1Sz563WM w2v/news_d200_e100.w2v.trainables.syn1neg.npy
-down large 1_GGMH8AuX-UNwiT6mJpF_h-S4YxxztuQ w2v/news_d200_e100.w2v.wv.vectors.npy
+download_from_gdrive 1oJZGdpu2Mm-Ga13H_5qxJeZ-nsHjn-NM w2v/news_d200_e100.w2v
+download_from_gdrive 11h5ZFbVAsqd41YnmM11TSZia1Sz563WM w2v/news_d200_e100.w2v.trainables.syn1neg.npy
+download_from_gdrive 1_GGMH8AuX-UNwiT6mJpF_h-S4YxxztuQ w2v/news_d200_e100.w2v.wv.vectors.npy
 
 # install python requirements
 pip3.6 install -r requirements.txt
